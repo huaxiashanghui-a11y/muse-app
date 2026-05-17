5af7d070fe50b795f72471eab8fff4fd3393b076
wuyoufuwu88-website <huaxiashanghui@gmail.com>
2026-05-17 13:57:10 +0800

    feat: add src/App.vue

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <router-view v-slot="{ Component, route }">
+    <keep-alive :include="['HomeIndex','FoodIndex','ActivityIndex','ShopIndex','ProfileIndex']">
+      <component :is="Component" :key="route.path" />
+    </keep-alive>
+  </router-view>
+</template>
